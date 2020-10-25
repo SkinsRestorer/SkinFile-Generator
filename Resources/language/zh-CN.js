9ba72197124d7f7da72c18a8b6887a32680bd6e2
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-10-25 23:15:04 +0800

    Add Simplified Chinese translate

---
+++ Resources/language/zh-CN.js
@@ -0,0 +1,26 @@
+let languageConfig = {
+    // DO NOT edit text not belong to translate section
+    // Start from here is the translate section
+    // 不要修改翻译区域以外部分的内容
+    // 从这里开始是翻译区域
+    "applicationName": "皮肤文件生成器",
+    "applicationIntroduction": "转换到这个插件支持的皮肤格式：",
+    "applicationAuthor": "原作者：",
+    "uploaderSelectSkinFile": "选择一个皮肤文件！",
+    "uploaderChooseSkinPlaceholder": "浏览文件…",
+    "uploaderSkinType": "皮肤分类",
+    "uploaderSkinTypeSteve": "史蒂夫",
+    "uploaderSkinTypeAlex": "艾里克斯",
+    "uploaderFileName": "[可选]为皮肤命名",
+    "uploaderSubmit": "转换！",
+    "apiInformationPowered": "赞助商：",
+    "informationImVeryHappy": "你想帮助这个项目的话我会很开心",
+    "informationHopeHelpUs": "我们希望你能帮助我们。",
+    "informationLol": "23333",
+    "informationDonate": "捐款",
+    "footerPowered": "赞助商："
+    // End of translate section
+    // 从这里结束翻译区域
+};
+
+languagePatch.apply(languageConfig);
