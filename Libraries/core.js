b170ad301bd0fa8be65be0858e198a33f9145e17
RiFlowTH <riflowth@gmail.com>
2021-01-16 02:47:06 +0700

    Force lowercase

--- Libraries/core.js
+++ Libraries/core.js
@@ -15,22 +15,23 @@ function upload(data, retrycount) {
     contentType: false,
     dataType: 'json',
     encode: true
-  }).done(function(response){
+  }).done(function (response) {
     var signature = response.data.texture.signature;
     var value = response.data.texture.value;
-    if(signature && value){
+    if (signature && value) {
       swal({
         type: 'success',
         title: 'Enjoy your skin',
         text: 'The System going to download a skin file!'
       });
       /* Create File */
-      var blob = new Blob([value+'\n'+signature+'\n'+4102444800000], {type: "text/plain;charset=utf-8"});
-      saveAs(blob, $('#fileName').val() == '' ? response.id + '.skin' : $('#fileName').val() + '.skin');
+      let fileName = $('#fileName').val().toLowerCase();
+      var blob = new Blob([value + '\n' + signature + '\n' + 4102444800000], { type: "text/plain;charset=utf-8" });
+      saveAs(blob, fileName == '' ? response.id + '.skin' : fileName + '.skin');
       /* ----------- */
     } else {
-      if (retrycount>0) {
-        upload(data, retrycount-1)
+      if (retrycount > 0) {
+        upload(data, retrycount - 1)
       } else {
         swal({
           type: 'error',
@@ -42,10 +43,10 @@ function upload(data, retrycount) {
 
     $('.custom-file-input').val('');
     $('.custom-file-label').html('Choose skins(s)...');
-  }).fail(function(response){
+  }).fail(function (response) {
     console.log('Fail : ' + response);
-    if (retrycount>0) {
-      upload(data, retrycount-1)
+    if (retrycount > 0) {
+      upload(data, retrycount - 1)
     } else {
       swal({
         type: 'error',
@@ -56,10 +57,9 @@ function upload(data, retrycount) {
   });
 }
 
-
-$('.custom-file-input').on('change', function(){
+$('.custom-file-input').on('change', function () {
   fileName = $(this).val().split('\\').pop();
-  if(fileName){
+  if (fileName) {
     $(this).next('.custom-file-label').html(fileName);
     console.log('File selected : ' + fileName);
   } else {
@@ -68,16 +68,16 @@ $('.custom-file-input').on('change', function(){
   }
 
   skinURL = URL.createObjectURL(event.target.files[0]);
-  skinChecker(function(){
+  skinChecker(function () {
     console.log('slimness: ' + isSlim);
     $("#skintype-alex").prop("checked", isSlim);
     $("#skintype-steve").prop("checked", !isSlim);
   });
 });
 
-$('#uploadFile').on('submit', function(e){
+$('#uploadFile').on('submit', function (e) {
   e.preventDefault();
-  if($('.custom-file-input').val()){
+  if ($('.custom-file-input').val()) {
     var data = new FormData($(this)[0]);
     upload(data, 2);
   } else {
@@ -90,12 +90,12 @@ $('#uploadFile').on('submit', function(e){
 });
 
 /* Check what type of skins (Alex or Steve) */
-function skinChecker(callback){
+function skinChecker(callback) {
   var image = new Image();
   image.crossOrigin = "Anonymous";
   image.src = skinURL;
 
-  image.onload = function(){
+  image.onload = function () {
     var detectCanvas = document.createElement("canvas");
     var detectCtx = detectCanvas.getContext("2d");
     detectCanvas.width = image.width;
@@ -104,8 +104,8 @@ function skinChecker(callback){
     var px1 = detectCtx.getImageData(46, 52, 1, 12).data;
     var px2 = detectCtx.getImageData(54, 20, 1, 12).data;
     var allTransparent = true;
-    for(var i = 3; i < 12 * 4; i += 4){
-      if(px1[i] === 255){
+    for (var i = 3; i < 12 * 4; i += 4) {
+      if (px1[i] === 255) {
         allTransparent = false;
         break;
       }
@@ -115,15 +115,15 @@ function skinChecker(callback){
       }
     }
     isSlim = allTransparent;
-    if(callback !== undefined){ callback(); }
+    if (callback !== undefined) { callback(); }
   }
 }
 
 /* If user changes skintype radios */
-$("[id^=skintype-]").on("change", function(){
+$("[id^=skintype-]").on("change", function () {
   isSlim = !isSlim;
 
-  skinChecker(function(){
+  skinChecker(function () {
     console.log('slimness: ' + isSlim);
     $("#skintype-alex").prop("checked", isSlim);
     $("#skintype-steve").prop("checked", !isSlim);
@@ -137,7 +137,7 @@ function checkApiStatus() {
     url: 'https://api.mineskin.org/',
     dataType: 'json',
     encode: true
-  }).fail(function(response){
+  }).fail(function (response) {
     $('#api-status-badge').html('DOWN');
     $('#api-status-badge').removeClass('badge-success');
     $('#api-status-badge').addClass('badge-danger');
